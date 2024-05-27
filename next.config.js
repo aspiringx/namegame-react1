c5d52805a899d4c5cf80db3525c25d0a5fd79eb7
Joe Tippetts <joe@fonespark.com>
2024-05-27 16:47:34 -0600

    Add github action files to publish to pages.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  // Allows deploy to github pages path.
+  basePath: "/namegame-react1",
   images: {
     remotePatterns: [
       {
